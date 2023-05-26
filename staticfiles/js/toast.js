dedd7d617d014b52d24e4ca2af83f52e6e1ea921
Raghav Bajaj <bajajraghav9@gmail.com>
2023-05-26 22:06:31 +0530

    images upload

---
+++ staticfiles/js/toast.js
@@ -0,0 +1,16 @@
+window.onload = (event) => {
+    showToast()
+}
+
+let x;
+let toast = document.getElementById("toast");
+function showToast(){
+    clearTimeout(x);
+    toast.style.transform = "translateX(0)";
+    x = setTimeout(()=>{
+        toast.style.transform = "translateX(100%)"
+    }, 60000);
+}
+function closeToast(){
+    toast.style.transform = "translateX(100%)";
+}
