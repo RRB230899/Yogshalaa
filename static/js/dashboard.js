4b49dfcb54dce4e9d5a6904ea76506477c9559df
Raghav Bajaj <bajajraghav9@gmail.com>
2023-05-26 23:06:13 +0530

    Billing toggle update

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -5,13 +5,13 @@
 //    nav[1].classList.toggle("active");
 //});
 
-window.onload = function() {
-var planBtn = document.getElementById("custom-checkbox");
-var plans = document.querySelectorAll(".pricing-body-plans > div");
-
-if (planBtn) {
-    planBtn.addEventListener("click", function() {
-        this.classList.toggle("quarterly");
-        plans[0].classList.toggle("active");
-        plans[1].classList.toggle("active");
-})}};
+//window.onload = (event) => {
+//var planBtn = document.getElementById("custom-checkbox");
+//var plans = document.querySelectorAll(".pricing-body-plans > div");
+//
+//if (planBtn) {
+//    planBtn.addEventListener("click", function() {
+//        this.classList.toggle("quarterly");
+//        plans[0].classList.toggle("active");
+//        plans[1].classList.toggle("active");
+//})};
--- static/js/toast.js
+++ static/js/toast.js
@@ -1,4 +1,13 @@
 window.onload = (event) => {
+    var planBtn = document.getElementById("custom-checkbox");
+    var plans = document.querySelectorAll(".pricing-body-plans > div");
+    if (planBtn){
+        planBtn.addEventListener("click", function() {
+        this.classList.toggle("quarterly");
+        plans[0].classList.toggle("active");
+        plans[1].classList.toggle("active");
+})
+    }
     showToast()
 }
 
