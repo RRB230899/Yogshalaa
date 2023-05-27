43850b86c1764724d64d732cb88ccd3672bdcd31
Raghav Bajaj <bajajraghav9@gmail.com>
2023-05-27 07:59:23 +0530

    Class Coverage & Dashboard prod changes

--- static/js/toast.js
+++ static/js/toast.js
@@ -7,20 +7,20 @@ window.onload = (event) => {
         plans[0].classList.toggle("active");
         plans[1].classList.toggle("active");
 })
-    }
-    showToast()
+//    }
+//    showToast()
 }
 
-let x;
-let toast = document.getElementById("toast");
-
-function showToast(){
-    clearTimeout(x);
-    toast.style.transform = "translateX(0)";
-    x = setTimeout(()=>{
-        toast.style.transform = "translateX(100%)"
-    }, 60000);
-}
-function closeToast(){
-    toast.style.transform = "translateX(100%)";
-}
+//let x;
+//let toast = document.getElementById("toast");
+//
+//function showToast(){
+//    clearTimeout(x);
+//    toast.style.transform = "translateX(0)";
+//    x = setTimeout(()=>{
+//        toast.style.transform = "translateX(100%)"
+//    }, 60000);
+//}
+//function closeToast(){
+//    toast.style.transform = "translateX(100%)";
+//}
