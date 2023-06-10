e514649190804f1bf907b479c3e1de691e840d92
Raghav Bajaj <bajajraghav9@gmail.com>
2023-06-10 23:08:07 +0530

    return statement

--- landing_page/helpers.py
+++ landing_page/helpers.py
@@ -1,5 +1,3 @@
-import requests
-import random
 from django.conf import settings
 from twilio.rest import Client
 
@@ -23,9 +21,9 @@ class OTPHandler:
             to=f'+{self.country_code}{self.phone_num}')
         print(f'Hello from Yogshalaa :) Your OTP for +{self.country_code}{self.phone_num} is {self.otp}. '
               f'This OTP is valid for 10 minutes only.')
+        return message
 
     # def send_otp_via_whatsapp(self):
     #     client = Client(settings.ACCOUNT_SID, settings.AUTH_TOKEN)
     #     message = client.messages.create(body=f'Your OTP is:{self.otp}', from_=f'{settings.TWILIO_WHATSAPP_NUMBER}',
     #                                      to=f'whatsapp:{settings.COUNTRY_CODE}{self.phone_num}')
-
