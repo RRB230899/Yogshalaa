769ca2665135081c20233750ee7f40ee2321f68b
Raghav Bajaj <bajajraghav9@gmail.com>
2023-06-19 19:18:25 +0530

    Gallery update

--- staticfiles/js/gallery.js
+++ staticfiles/js/gallery.js
@@ -6,7 +6,7 @@ gsap.timeline()
       rotateY: (i)=> i*-36,
       transformOrigin: '50% 50% 500px',
       z: -500+'px',
-      backgroundImage:(i)=>'url(https://picsum.photos/id/'+(i+32)+'/600/400/)',
+      backgroundImage:(i)=>'url(static/gallery/'+(i+1)+'.jpg)',
       backgroundPosition:(i)=>getBgPos(i),
       backfaceVisibility:'hidden'
     })
