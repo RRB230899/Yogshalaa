7b5b0bbebd99cec57399be21d2b9ec5b8fd7ffad
RRB230899 <bajajraghav9@gmail.com>
2025-09-11 03:39:50 -0400

    Static Replacement

--- yogshalaa/settings.py
+++ yogshalaa/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["yogshalaa.in", '127.0.0.1', 'yogshalaa.onrender.com', '*']
 
